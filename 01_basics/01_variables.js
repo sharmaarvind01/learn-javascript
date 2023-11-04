17fd1f9c78f9a90df2d3292b85ea70017eabfae5
Arvind Sharma <sharma.arvind@neosoftmail.com>
2023-11-05 00:44:00 +0530

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const userId = 123455
+let userEmail = "arvind@ars.com"
+var userPass = "arvind"
+city  = "mumbai"
+
+/**
+ * here are some examples of variables in javasctipt that you can use and you should
+ * avoid to use
+ * where data type const where you are not allowd to resign value
+ * and data type let is what you can use as block level declaration of value and you can reassign it
+ * and var is a global scope variable which you should avaoid to use it 
+ * not declared data type which you should avoid useing it this is not good for storing data
+ * 
+ * */
+// userId = 23245
+console.log(city)
+console.table([userId, userEmail, userPass, city])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js as newer version
+
+// alert("hello") // we are useing Node.js 
+
+let name = "arvind sharma"
+let age = 26
+let isLoggedIn = false
+let state
+
+// Number -> 2 to power of 53
+// bigint
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined 
+// symbol -> unique
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // Object
