43131b41c306c283905b01d0ac5cfbb3d8ce7de8
Arvind Sharma <sharma.arvind@neosoftmail.com>
2024-01-22 23:47:13 +0530

    color -flipper

---
+++ mini-projects/color-flipper/scripts.js
@@ -0,0 +1,47 @@
+/**
+*Color flipper Scripts
+*/
+
+const body = document.body;
+const button = document.querySelector('.color-button');
+body.style.backgroundColor = "red";
+const displayName = document.querySelector('#color-name');  
+const colorNames = [
+    "aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond",
+    "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue",
+    "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki",
+    "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue",
+    "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick",
+    "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow",
+    "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon",
+    "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink", "lightsalmon",
+    "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen",
+    "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen",
+    "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream",
+    "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid",
+    "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum",
+    "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown",
+    "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue",
+    "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"
+  ];
+
+  // change color on button click
+
+button.addEventListener('click',(e)=>{
+    e.preventDefault;
+    let rand = Math.floor(Math.random()*141);
+    displayName.textContent = colorNames[rand].toUpperCase();
+    displayName.style.color = colorNames[Math.floor(Math.random()*141)];
+    body.style.backgroundColor = colorNames[rand];
+    button.style.borderColor = colorNames[rand];
+})
+
+/* Auto color change
+setInterval((e)=>{
+    let rand = Math.floor(Math.random()*141);
+    displayName.textContent = colorNames[rand].toUpperCase();
+    body.style.backgroundColor = colorNames[rand];
+    button.style.borderColor = colorNames[rand+1];
+},1000)
+
+*/
